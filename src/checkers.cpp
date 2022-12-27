3ba04844b8bc9fd4603a7086f2b283ca431d6a6a
Yagiz Nizipli <yagiz@nizipli.com>
2022-12-27 09:15:00 -0500

    feat: add is_ipv4_number_valid

--- src/checkers.cpp
+++ src/checkers.cpp
@@ -2,6 +2,7 @@
 #include "unicode.cpp"
 
 #include <algorithm>
+#include <cstring>
 
 namespace ada::checkers {
 
@@ -36,7 +37,7 @@ namespace ada::checkers {
     }
 
     // If parsing last as an IPv4 number does not return failure, then return true.
-    return ada::parser::parse_ipv4_number(last).has_value();
+    return ada::checkers::is_ipv4_number_valid(last);
   }
 
   // A Windows drive letter is two code points, of which the first is an ASCII alpha
@@ -48,6 +49,37 @@ namespace ada::checkers {
   // A normalized Windows drive letter is a Windows drive letter of which the second code point is U+003A (:).
   bool is_normalized_windows_drive_letter(std::string_view input) {
     return is_windows_drive_letter(input) && input[1] == ':';
+  }
+
+  bool is_ipv4_number_valid(std::string_view input) {
+    // If input is the empty string, then return failure.
+    if (input.empty()) {
+      return false;
+    }
+
+    if (input.length() >= 2) {
+      // If input contains at least two code points and the first two code points are either "0X" or "0x", then:
+      if (input[0] == '0' && (input[1] == 'X' || input[1] == 'x')) {
+        // Remove the first two code points from input.
+        input.remove_prefix(2);
+
+        // If input is the empty string, then return (0, true).
+        if (input.empty()) {
+          return true;
+        }
+
+        // If input contains a code point that is not a radix-R digit, then return failure.
+        return std::strspn(input.data(), "0123456789abcdefABCDEF") >= input.length();
+      }
+      // Otherwise, if input contains at least two code points and the first code point is U+0030 (0), then:
+      else if (input[1] == '0') {
+        // Remove the first code point from input.
+        input.remove_prefix(1);
+      }
+    }
+
+    // If input contains a code point that is not a radix-R digit, then return failure.
+    return std::strspn(input.data(), "0123456789") >= input.length();
   }
 
 } // namespace ada::checkers
