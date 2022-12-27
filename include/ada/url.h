2c209cd7457d32e17410c46b10a56a135e0402d7
Yagiz Nizipli <yagiz@nizipli.com>
2022-12-27 14:02:03 -0500

    fix: change variable declaration

--- include/ada/url.h
+++ include/ada/url.h
@@ -96,7 +96,7 @@ namespace ada {
     /**
      * A URL is special if its scheme is a special scheme. A URL is not special if its scheme is not a special scheme.
      */
-    bool is_special = false;
+    bool is_special{false};
 
     /**
      * A URL has an opaque path if its path is a string.
