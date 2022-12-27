a69205832dc52396aa8f1d5e25c92c618a0fd214
Yagiz Nizipli <yagiz@nizipli.com>
2022-12-27 09:15:00 -0500

    feat: add is_ipv4_number_valid

--- include/ada/checkers.h
+++ include/ada/checkers.h
@@ -10,6 +10,7 @@ namespace ada::checkers {
   bool ends_in_a_number(std::string_view input);
   bool is_windows_drive_letter(std::string_view input);
   bool is_normalized_windows_drive_letter(std::string_view input);
+  bool is_ipv4_number_valid(std::string_view input);
 
 } // namespace ada::checkers
 
