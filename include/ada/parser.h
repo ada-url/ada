2f03aec1fdc335025c5b0298717f0c49c775ea6d
Yagiz Nizipli <yagiz@nizipli.com>
2022-12-30 16:58:42 -0500

    fix: improve edge cases

--- include/ada/parser.h
+++ include/ada/parser.h
@@ -17,7 +17,7 @@ namespace ada::parser {
   std::optional<std::string_view> parse_ipv6(std::string_view input);
   std::optional<std::string_view> parse_host(std::string_view input, bool is_not_special);
 
-  url parse_url(std::string_view user_input,
+  url parse_url(std::string user_input,
                 std::optional<ada::url> base_url,
                 ada::encoding_type encoding = UTF8,
                 std::optional<ada::state> state_override = std::nullopt);
