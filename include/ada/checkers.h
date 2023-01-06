f3a185478f587270c3528602e142da586338e620
Yagiz Nizipli <yagiz@nizipli.com>
2023-01-06 18:18:22 -0500

    perf: improve ends_in_a_number by 25%

--- include/ada/checkers.h
+++ include/ada/checkers.h
@@ -10,6 +10,7 @@ namespace ada::checkers {
   bool ends_in_a_number(std::string_view input) noexcept;
   bool is_windows_drive_letter(std::string_view input) noexcept;
   bool is_normalized_windows_drive_letter(std::string_view input) noexcept;
+  ada_really_inline constexpr bool is_ipv4_number_valid(const std::string_view input) noexcept;
 
 } // namespace ada::checkers
 
