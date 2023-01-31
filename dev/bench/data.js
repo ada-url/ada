f1373f4e2d7616f404a019d4443e6506a0a3fc40
github-action-benchmark <github@users.noreply.github.com>
2023-01-31 15:50:00 +0000

    add Web Platform Tests (googlecpp) benchmark result for 15a0a5fd29a7a9c0c48ec2da06c7ca9ef2a36e73

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1675180197568,
+  "lastUpdate": 1675180200584,
   "repoUrl": "https://github.com/ada-url/ada",
   "entries": {
     "Benchmark": [
@@ -30,6 +30,38 @@ window.BENCHMARK_DATA = {
             "value": 5976.712125449837,
             "unit": "ns/iter",
             "extra": "iterations: 110833\ncpu: 5964.65944258479 ns\nthreads: 1"
+          }
+        ]
+      }
+    ],
+    "Web Platform Tests": [
+      {
+        "commit": {
+          "author": {
+            "email": "yagiz@nizipli.com",
+            "name": "Yagiz Nizipli",
+            "username": "anonrig"
+          },
+          "committer": {
+            "email": "yagiz@nizipli.com",
+            "name": "Yagiz Nizipli",
+            "username": "anonrig"
+          },
+          "distinct": true,
+          "id": "15a0a5fd29a7a9c0c48ec2da06c7ca9ef2a36e73",
+          "message": "build: add continious benchmark CI",
+          "timestamp": "2023-01-31T10:47:05-05:00",
+          "tree_id": "f9a57cc31bffda9d0d39b8845729a4a3fb9e4f0d",
+          "url": "https://github.com/ada-url/ada/commit/15a0a5fd29a7a9c0c48ec2da06c7ca9ef2a36e73"
+        },
+        "date": 1675180199983,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BasicBench_AdaURL",
+            "value": 498029.16417911195,
+            "unit": "ns/iter",
+            "extra": "iterations: 1407\ncpu: 497900.7107320541 ns\nthreads: 1"
           }
         ]
       }
