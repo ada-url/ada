45bd3c3c12343710e40a99f0f7af6b7ef2a4ff1c
github-action-benchmark <github@users.noreply.github.com>
2023-01-31 15:58:37 +0000

    add BBC URLs (googlecpp) benchmark result for 96a6d8b8f32f201800717d7eefbabd99afefef27

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1675180714539,
+  "lastUpdate": 1675180717527,
   "repoUrl": "https://github.com/ada-url/ada",
   "entries": {
     "Benchmark": [
@@ -62,6 +62,38 @@ window.BENCHMARK_DATA = {
             "value": 460503.4683462534,
             "unit": "ns/iter",
             "extra": "iterations: 1548\ncpu: 459780.1679586563 ns\nthreads: 1"
+          }
+        ]
+      }
+    ],
+    "BBC URLs": [
+      {
+        "commit": {
+          "author": {
+            "email": "yagiz@nizipli.com",
+            "name": "Yagiz Nizipli",
+            "username": "anonrig"
+          },
+          "committer": {
+            "email": "yagiz@nizipli.com",
+            "name": "Yagiz Nizipli",
+            "username": "anonrig"
+          },
+          "distinct": true,
+          "id": "96a6d8b8f32f201800717d7eefbabd99afefef27",
+          "message": "build: add sudo prefix to benchmarks",
+          "timestamp": "2023-01-31T10:56:19-05:00",
+          "tree_id": "c44d7425bd152956262f7c97d4e140e24a9f4cdc",
+          "url": "https://github.com/ada-url/ada/commit/96a6d8b8f32f201800717d7eefbabd99afefef27"
+        },
+        "date": 1675180716608,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BasicBench_AdaURL",
+            "value": 4899.055577989013,
+            "unit": "ns/iter",
+            "extra": "iterations: 136205\ncpu: 4898.441320069013 ns\nthreads: 1"
           }
         ]
       }
