dc91aeef807672cdaba82529c27dcc39ebeb6f36
github-action-benchmark <github@users.noreply.github.com>
2023-01-31 16:06:05 +0000

    add BBC URLs (googlecpp) benchmark result for 06751609bf4904a4504ab317ed631fd08582bda5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1675181160505,
+  "lastUpdate": 1675181165617,
   "repoUrl": "https://github.com/ada-url/ada",
   "entries": {
     "Benchmark": [
@@ -68,6 +68,44 @@ window.BENCHMARK_DATA = {
             "value": 414969.23885918246,
             "unit": "ns/iter",
             "extra": "iterations: 1683\ncpu: 414943.13725490205 ns\nthreads: 1"
+          }
+        ]
+      }
+    ],
+    "BBC URLs": [
+      {
+        "commit": {
+          "author": {
+            "email": "yagiz@nizipli.com",
+            "name": "Yagiz Nizipli",
+            "username": "anonrig"
+          },
+          "committer": {
+            "email": "yagiz@nizipli.com",
+            "name": "Yagiz Nizipli",
+            "username": "anonrig"
+          },
+          "distinct": true,
+          "id": "06751609bf4904a4504ab317ed631fd08582bda5",
+          "message": "build: add ccache, competitors and ninja to benchmark",
+          "timestamp": "2023-01-31T11:03:56-05:00",
+          "tree_id": "9a70561d36e8dcc47107bcf11f691001a68784df",
+          "url": "https://github.com/ada-url/ada/commit/06751609bf4904a4504ab317ed631fd08582bda5"
+        },
+        "date": 1675181163804,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BasicBench_AdaURL",
+            "value": 4145.726397465734,
+            "unit": "ns/iter",
+            "extra": "iterations: 168573\ncpu: 4145.342967141831 ns\nthreads: 1"
+          },
+          {
+            "name": "BasicBench_CURL",
+            "value": 5812.565289682068,
+            "unit": "ns/iter",
+            "extra": "iterations: 120287\ncpu: 5812.03787607971 ns\nthreads: 1"
           }
         ]
       }
