0958447145fc745d649785dc94f6368ecf707e9d
github-action-benchmark <github@users.noreply.github.com>
2023-01-31 15:32:39 +0000

    add C++ Benchmark (googlecpp) benchmark result for 6172762251b1e79ec08002a2e4bc53c0c87bb6b4

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1675179159754,
+  "repoUrl": "https://github.com/ada-url/ada",
+  "entries": {
+    "C++ Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "yagiz@nizipli.com",
+            "name": "Yagiz Nizipli",
+            "username": "anonrig"
+          },
+          "committer": {
+            "email": "yagiz@nizipli.com",
+            "name": "Yagiz Nizipli",
+            "username": "anonrig"
+          },
+          "distinct": true,
+          "id": "6172762251b1e79ec08002a2e4bc53c0c87bb6b4",
+          "message": "build: add continious benchmark CI",
+          "timestamp": "2023-01-31T10:31:27-05:00",
+          "tree_id": "4e3826cda4c01dcb025092773b7751a2b889ab18",
+          "url": "https://github.com/ada-url/ada/commit/6172762251b1e79ec08002a2e4bc53c0c87bb6b4"
+        },
+        "date": 1675179158618,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BasicBench_AdaURL",
+            "value": 415977.11255923734,
+            "unit": "ns/iter",
+            "extra": "iterations: 1688\ncpu: 414766.9431279622 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
