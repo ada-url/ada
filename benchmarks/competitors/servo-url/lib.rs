f21aa5daf5fd44c5e4886ce637c2bbd878887ce4
Yagiz Nizipli <yagiz@nizipli.com>
2023-02-20 08:37:31 -0500

    fix: improve performance of servo

--- benchmarks/competitors/servo-url/lib.rs
+++ benchmarks/competitors/servo-url/lib.rs
@@ -1,21 +1,20 @@
 use url::Url;
-use std::ffi::{CString};
 use std::{slice, ptr};
-use libc::{c_char};
+use libc::c_char;
 
 extern crate url;
 extern crate libc;
 
 pub struct StandardUrl {
   pub port: u16,
-  pub scheme: *mut c_char,
-  pub username: *mut c_char,
-  pub password: *mut c_char,
-  pub host: *mut c_char,
-  pub query: *mut c_char,
-  pub fragment: *mut c_char,
-  pub path: *mut c_char,
-  pub href: *mut c_char,
+  pub scheme: String,
+  pub username: String,
+  pub password: String,
+  pub host: String,
+  pub query: String,
+  pub fragment: String,
+  pub path: String,
+  pub href: String,
 }
 
 #[no_mangle]
@@ -25,25 +24,16 @@ pub extern "C" fn parse_url(raw_input: *const c_char, raw_input_length: usize) -
   };
   let result = Url::parse(input).unwrap();
 
-  let scheme = CString::new(result.scheme()).unwrap().into_raw();
-  let username = CString::new(result.username()).unwrap().into_raw();
-  let password = CString::new(result.password().unwrap_or("")).unwrap().into_raw();
-  let host = CString::new(result.host_str().unwrap_or("")).unwrap().into_raw();
-  let query = CString::new(result.query().unwrap_or("")).unwrap().into_raw();
-  let fragment = CString::new(result.fragment().unwrap_or("")).unwrap().into_raw();
-  let path = CString::new(result.path().to_string()).unwrap().into_raw();
-  let href = CString::new(result.as_str()).unwrap().into_raw();
-
   let mut out = StandardUrl {
       port: result.port().unwrap_or(0),
-      scheme,
-      username,
-      password,
-      host,
-      query,
-      fragment,
-      path,
-      href,
+      scheme: result.scheme().to_string(),
+      username: result.username().to_string(),
+      password: result.password().unwrap_or("").to_string(),
+      host: result.host_str().unwrap_or("").to_string(),
+      query: result.query().unwrap_or("").to_string(),
+      fragment: result.fragment().unwrap_or("").to_string(),
+      path: result.path().to_string(),
+      href: result.as_str().to_string(),
   };
 
   ptr::addr_of_mut!(out)
