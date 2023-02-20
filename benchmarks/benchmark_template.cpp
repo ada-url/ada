83f2beb6559be981e54509067c40fa8218fcaae6
Yagiz Nizipli <yagiz@nizipli.com>
2023-02-19 19:14:22 -0500

    add servo-url to benchmarks

--- benchmarks/benchmark_template.cpp
+++ benchmarks/benchmark_template.cpp
@@ -549,3 +549,48 @@ int main(int argc, char **argv) {
     benchmark::RunSpecifiedBenchmarks();
     benchmark::Shutdown();
 }
+
+#include "competitors/servo-url/servo_url.h"
+
+static void BasicBench_ServoUrl(benchmark::State& state) {
+  // volatile to prevent optimizations.
+  for (auto _ : state) {
+    for(std::string& url_string : url_examples) {
+      benchmark::DoNotOptimize(servo_url::parse_url(url_string.c_str()));
+    }
+  }
+  if(collector.has_events()) {
+    event_aggregate aggregate{};
+    for(size_t i = 0 ; i < N; i++) {
+      std::atomic_thread_fence(std::memory_order_acquire);
+      collector.start();
+      for(std::string& url_string : url_examples) {
+        benchmark::DoNotOptimize(servo_url::parse_url(url_string.c_str()));
+      }
+      std::atomic_thread_fence(std::memory_order_release);
+      event_count allocate_count = collector.end();
+      aggregate << allocate_count;
+    }
+    state.counters["cycles/url"] = aggregate.best.cycles() / std::size(url_examples);
+    state.counters["instructions/url"] = aggregate.best.instructions() / std::size(url_examples);
+    state.counters["instructions/cycle"] = aggregate.best.instructions() / aggregate.best.cycles();
+    state.counters["instructions/byte"] = aggregate.best.instructions() / url_examples_bytes;
+    state.counters["instructions/ns"] = aggregate.best.instructions() / aggregate.best.elapsed_ns();
+    state.counters["GHz"] = aggregate.best.cycles() / aggregate.best.elapsed_ns();
+    state.counters["ns/url"] = aggregate.best.elapsed_ns() / std::size(url_examples);
+    state.counters["cycle/byte"] = aggregate.best.cycles() / url_examples_bytes;
+  }
+
+  state.counters["time/byte"] = benchmark::Counter(
+      url_examples_bytes,
+      benchmark::Counter::kIsIterationInvariantRate | benchmark::Counter::kInvert);
+  state.counters["time/url"] = benchmark::Counter(
+      double(std::size(url_examples)),
+      benchmark::Counter::kIsIterationInvariantRate | benchmark::Counter::kInvert);
+  state.counters["speed"] = benchmark::Counter(
+      url_examples_bytes, benchmark::Counter::kIsIterationInvariantRate);
+  state.counters["url/s"] = benchmark::Counter(
+      double(std::size(url_examples)),
+      benchmark::Counter::kIsIterationInvariantRate);
+}
+BENCHMARK(BasicBench_ServoUrl);
