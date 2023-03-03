c5e55c558cafd17924c021c5b99f9cf800944069
Daniel Lemire <daniel@lemire.me>
2023-03-03 15:30:23 -0500

    fix: standardize the benchmarks

--- benchmarks/competitors/servo-url/servo_url.h
+++ benchmarks/competitors/servo-url/servo_url.h
@@ -22,6 +22,10 @@ Url *parse_url(const char *raw_input, size_t raw_input_length);
 
 void free_url(Url *raw);
 
+
+const char *parse_url_to_href(const char *raw_input, size_t raw_input_length);
+
+void free_string(const char *);
 } // extern "C"
 
 } // namespace servo_url
