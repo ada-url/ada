4d2a742ed841346526871fb01e2f4f68c03432ad
Yagiz Nizipli <yagiz@nizipli.com>
2023-03-03 10:24:25 -0500

    docs: update url_components documentation

--- include/ada/url_components.h
+++ include/ada/url_components.h
@@ -1,5 +1,5 @@
 /**
- * @file url-components.h
+ * @file url_components.h
  * @brief Declaration for the URL Components
  */
 #ifndef ADA_URL_COMPONENTS_H
@@ -14,8 +14,9 @@ namespace ada {
 
   /**
    * We design the url_components struct so that it is as small
-   * and simple as possible. This version uses 32 bytes. A version with size_t
-   * and std::optional<size_t> might use 80 bytes or more.
+   * and simple as possible. This version uses 32 bytes.
+   *
+   * This struct is used to extract components from a single 'href'.
    */
   struct url_components {
     constexpr static uint32_t omitted = uint32_t(-1);
@@ -26,9 +27,21 @@ namespace ada {
     url_components &operator=(url_components &&u) noexcept = default;
     url_components &operator=(const url_components &u) = default;
     ~url_components() = default;
+
     /*
      * By using 32-bit integers, we implicitly assume that the URL string
      * cannot exceed 4 GB.
+     *
+     * https://user@pass:example.com:1234/foo/bar?baz#quux
+     *      |      |    |          | ^^^^|       |   |
+     *      |      |    |          | |   |       |   `----- hash_start
+     *      |      |    |          | |   |       `--------- search_start
+     *      |      |    |          | |   `----------------- pathname_start
+     *      |      |    |          | `--------------------- port
+     *      |      |    |          `----------------------- host_end
+     *      |      |    `---------------------------------- host_start
+     *      |      `--------------------------------------- username_end
+     *      `---------------------------------------------- protocol_end
      */
     uint32_t protocol_end{0};
     uint32_t username_end{0};
@@ -38,21 +51,11 @@ namespace ada {
     uint32_t pathname_start{0};
     uint32_t search_start{omitted};
     uint32_t hash_start{omitted};
-     /**
-      *
-      * https://user@pass:example.com:1234/foo/bar?baz#quux
-      *      |      |    |          | ^^^^|       |   |
-      *      |      |    |          | |   |       |   `----- hash_start
-      *      |      |    |          | |   |       `--------- search_start
-      *      |      |    |          | |   `----------------- pathname_start
-      *      |      |    |          | `--------------------- port
-      *      |      |    |          `----------------------- host_end
-      *      |      |    `---------------------------------- host_start
-      *      |      `--------------------------------------- username_end
-      *      `---------------------------------------------- protocol_end
-      */
 
-
+    /**
+     * @private
+     * Converts a url_components to JSON stringified version.
+     */
     std::string to_string() const;
 
   }; // struct url_components
