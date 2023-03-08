c440a49cb00b3f8cc86e0c3256f8b427d22796f5
Miguel Teixeira <miguel.teixeira@poli.ufrj.br>
2023-03-07 21:08:01 -0300

    cmake|tools: adding formatting scripts

---
+++ tools/lint_and_format.py
@@ -0,0 +1,72 @@
+import os
+import sys
+import subprocess
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="Format C/C++ code using clang-format."
+)
+parser.add_argument(
+    "operation",
+    choices=["check", "format"],
+    help='Operation to perform: "check" to check for formatting errors, or "format" to fix formatting errors.',
+)
+parser.add_argument(
+    "--extensions",
+    "-e",
+    nargs="+",
+    default=[".cpp", ".cc", ".c", ".h", ".hpp"],
+    help="List of file extensions to check or format (default: .cpp .cc .c .h .hpp)",
+)
+args = parser.parse_args()
+
+ROOT_DIR = (
+    subprocess.check_output(["git", "rev-parse", "--show-toplevel"])
+    .strip()
+    .decode("utf-8")
+)
+
+exclude_dirs = ('.git', '.cache', 'build')
+file_list = [os.path.join(dirpath, filename)
+             for dirpath, _, filenames in os.walk(ROOT_DIR)
+             for filename in filenames
+             if any(filename.endswith(ext) for ext in args.extensions)
+             and not any(exclude_dir in dirpath for exclude_dir in exclude_dirs)]
+
+
+def clang_check(file_path: str) -> None:
+    try:
+        diff_output = subprocess.check_output(
+            ["clang-format", "-output-replacements-xml", file_path], stderr=subprocess.STDOUT,
+        )
+        if b"<replacement " in diff_output:
+            print(f"Error: {file_path} needs formatting")
+            sys.exit(1)
+
+    except subprocess.CalledProcessError as error:
+        print(f'Error: {error.output.decode("utf-8")}')
+        sys.exit(1)
+
+
+def clang_format(file_path: str) -> None:
+    diff_output = subprocess.check_output(
+        ["clang-format", "-output-replacements-xml", file_path], stderr=subprocess.STDOUT,
+    )
+
+    if b"<replacement " in diff_output:
+        print(f"Formatting: {file_path}")
+        try:
+            subprocess.check_call(["clang-format", "-i", file_path])
+        except subprocess.CalledProcessError as error:
+            print(f'Error: {error.output.decode("utf-8")}')
+            sys.exit(1)
+
+
+for file_path in file_list:
+    if args.operation == "check":
+        clang_check(file_path)
+    elif args.operation == "format":
+        clang_format(file_path)
+
+
+print("Done!")
