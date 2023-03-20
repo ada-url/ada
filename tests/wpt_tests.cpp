82fd3832f9d5cf9cccbd7ec4f9c2566b62183bea
Yagiz Nizipli <yagiz@nizipli.com>
2023-03-20 17:28:46 -0400

    fix: enable setters_tests_encoding tests for ada::url

--- tests/wpt_tests.cpp
+++ tests/wpt_tests.cpp
@@ -558,7 +558,7 @@ int main(int argc, char** argv) {
 #endif // !ADA_HAS_ICU
   }
   name = "setters_tests_encoding<ada::url>("+std::string(ADA_SETTERS_TESTS_JSON)+")";
-  if(all_ada_url_aggregator_tests || name.find(filter) != std::string::npos) {
+  if(all_ada_url_tests || name.find(filter) != std::string::npos) {
     results[name] = setters_tests_encoding<ada::url>(ADA_SETTERS_TESTS_JSON);
     if(stop_on_failure && !results[name]) { exit(-1); }
 #if !ADA_HAS_ICU
