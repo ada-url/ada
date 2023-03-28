6c40513ca8157f640551b830c08f1c2cc5cc13ba
Daniel Lemire <daniel@lemire.me>
2023-03-27 21:54:27 -0400

    adding IDNA_TEST_V2

--- tests/wpt_tests.cpp
+++ tests/wpt_tests.cpp
@@ -14,9 +14,6 @@
 #include "ada/url.h"
 #include "ada/url_aggregator.h"
 
-// We think that these examples have bad domains.
-std::set<std::string> bad_domains = {"http://./", "http://../",
-                                     "http://foo.09.."};
 
 // This function copies your input onto a memory buffer that
 // has just the necessary size. This will entice tools to detect
@@ -49,6 +46,7 @@ const char *SETTERS_TESTS_JSON = WPT_DATA_DIR "setters_tests.json";
 const char *ADA_SETTERS_TESTS_JSON =
     WPT_DATA_DIR "ada_extra_setters_tests.json";
 const char *TOASCII_JSON = WPT_DATA_DIR "toascii.json";
+const char *IDNA_TEST_V2 = WPT_DATA_DIR "IdnaTestV2.json";
 const char *URLTESTDATA_JSON = WPT_DATA_DIR "urltestdata.json";
 const char *ADA_URLTESTDATA_JSON = WPT_DATA_DIR "ada_extra_urltestdata.json";
 const char *VERIFYDNSLENGTH_TESTS_JSON =
@@ -97,6 +95,69 @@ bool file_exists(const char *filename) {
     error_buffer << "  file missing: " << filename << std::endl;
     return false;
   }
+}
+
+
+bool idna_test_v2_to_ascii() {
+  TEST_START();
+  ondemand::parser parser;
+
+  RUN_TEST(file_exists(IDNA_TEST_V2));
+
+  padded_string json = padded_string::load(IDNA_TEST_V2);
+  std::cout << "  loaded " << IDNA_TEST_V2 << " (" << json.size() << " kB)"
+            << std::endl;
+
+  ondemand::document doc = parser.iterate(json);
+  try {
+    for (auto element : doc.get_array()) {
+        if (element.type() == ondemand::json_type::string) {
+            continue;
+        }
+
+        ondemand::object object = element.get_object();
+        auto json_string = std::string(std::string_view(simdjson::to_json_string(object)));
+
+        try {
+            auto comment = object["comment"];
+            if (comment) {
+                std::cout << "   comment: " << comment.get_string() << std::endl;
+            }
+        } catch (simdjson::simdjson_error ignored) {}
+
+        std::string_view input = object["input"].get_string();
+
+        std::optional<std::string> output;
+        ada::unicode::to_ascii(output, input, false, input.find('%'));
+
+        auto expected_output = object["output"];
+        auto given_output = output.has_value() ? output.value() : "";
+
+
+        if (expected_output.is_null()) {
+            TEST_ASSERT(
+              given_output,
+              "",
+              "\n  Test case: " + json_string +
+              "\n  Got output: " + given_output
+            );
+
+        } else if (expected_output.type() == ondemand::json_type::string) {
+            std::string_view str_expected_output = expected_output.get_string();
+            TEST_ASSERT(
+              str_expected_output,
+              given_output,
+              "\n  Test case: " + json_string +
+              "\n  Got output: " + given_output
+            );
+        }
+    }
+  } catch (simdjson::simdjson_error &error) {
+    std::cerr << "JSON error: " << error.what() << " near "
+              << doc.current_location() << " in " << TOASCII_JSON << std::endl;
+    return false;
+  }
+  TEST_SUCCEED();
 }
 
 bool percent_encoding() {
@@ -505,12 +566,6 @@ bool urltestdata_encoding(const char *source) {
             TEST_ASSERT(input_url->get_origin(), origin,
                         "Origin " + element_string + input_url->to_string());
           }
-          if (bad_domains.find(std::string(input)) != bad_domains.end()) {
-            TEST_ASSERT(
-                input_url->has_valid_domain(), false,
-                "Bad domain " + element_string + input_url->to_string());
-          }
-
           // We need padding.
           simdjson::padded_string padded_url_json = parsed_url_json;
           // We need a second parser.
@@ -615,6 +670,13 @@ int main(int argc, char **argv) {
   std::map<std::string, bool> results;
   std::string name;
 
+  name = "idna_test_v2_to_ascii";
+  if (all_ada_url_tests || name.find(filter) != std::string::npos) {
+    results[name] = idna_test_v2_to_ascii();
+    if (stop_on_failure && !results[name]) {
+      exit(-1);
+    }
+  }
   name = "urltestdata_encoding<ada::url>(" + std::string(ADA_URLTESTDATA_JSON) +
          ")";
   if (all_ada_url_tests || name.find(filter) != std::string::npos) {
