1ca5b56ff70f75ad7efec8efef4fc1782ea230bf
Miguel Teixeira <miguel.teixeira@poli.ufrj.br>
2023-04-01 11:17:13 -0300

    releases: refactoring get_new_contributors

--- tools/release/release.py
+++ tools/release/release.py
@@ -4,46 +4,68 @@ from github import Github
 import os
 
 
-def get_merged_pull_requests(repo, last_release):
-    pulls = repo.get_pulls(state='closed')
-    return [pull for pull in pulls if pull.merged and pull.merged_at > last_release.created_at]
+def get_release_merged_pulls(repository, last_release):
+    pulls = repository.get_pulls(state="closed")
+    return [
+        pull
+        for pull in pulls
+        if pull.merged and pull.merged_at > last_release.created_at
+    ]
 
 
-def get_new_contributors(repo, merged_pulls):
+def get_new_contributors(repository):
+    last_release = get_last_release(repository)
+    merged_pulls = [
+        pull
+        for pull in repository.get_pulls(state="closed")
+        if pull.merged and pull.merged_at <= last_release.created_at
+    ]
     contributors = set()
-    repo.get_contributors()
-    print(repo.get_contributors())
     for pull in merged_pulls:
+        contributors.add(pull.user.login)
+
+    new_contributors = set()
+    release_merged_pulls = get_release_merged_pulls(repository, last_release)
+    for pull in release_merged_pulls:
         contributor = pull.user.login
-        if contributor not in repo.get_contributors():
-            contributors.add(contributor)
-    return contributors
+        if contributor not in contributors:
+            new_contributors.add(contributor)
+    return new_contributors
 
 
-def whats_changed():
+def get_last_release(repository):
+    sorted_releases = sorted(
+        repository.get_releases(), key=lambda r: r.created_at, reverse=True
+    )
+
+    last_release = repository
+    if len(sorted_releases) >= 2:
+        last_release = sorted_releases[1]
+
+    return last_release
+
+
+def whats_changed(repository):
+    last_release = get_last_release(repository)
+    release_merged_pulls = get_release_merged_pulls(repository, last_release)
+    release_changes = ""
+    for pull in release_merged_pulls:
+        release_changes += f"{pull.title} by @{pull.user.login} in #{pull.number}\n"
+
+    release_changes += "\n"
+    return changes
+
+
+if __name__ == "__main__":
     repo_name = os.environ["GITHUB_REPOSITORY"]
     github_token = os.environ["GITHUB_TOKEN"]
+    if not repo_name or not github_token:
+        raise Exception(
+            f"Bad environment variables. Invalid GITHUB_REPOSITORY or GITHUB_TOKEN"
+        )
+
     g = Github(github_token)
     repo = g.get_repo(repo_name)
 
-    sorted_releases = sorted(repo.get_releases(), key=lambda r: r.created_at, reverse=True)
-    last_release = repo
-
-    if len(sorted_releases) >= 2:
-        previous_release = sorted_releases[1]
-        last_release = sorted_releases[1]
-        print(f"Previous release: {previous_release.tag_name}")
-    else:
-        print("No previous release found")
-
-    merged_pulls = get_merged_pull_requests(repo, last_release)
-
-    # get new contributors since last release
-    get_new_contributors(repo, merged_pulls)
-
-    # print details of merged pull requests since last release
-    for pull in merged_pulls:
-        print(f"{pull.title} by @{pull.user.login} in #{pull.number}")
-
-
-whats_changed()
+    changes = whats_changed(repo)
+    nc = get_new_contributors(repo)
