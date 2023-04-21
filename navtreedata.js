0a4ca4f3984bd40efed1d9afe44bda6cf0ee3473
anonrig <anonrig@users.noreply.github.com>
2023-04-21 20:36:12 +0000

    deploy: 82eff309bc75cc93cfe9e2871f7e5feb278d48e4

--- navtreedata.js
+++ navtreedata.js
@@ -31,10 +31,13 @@ var NAVTREE =
       [ "Examples", "index.html#autotoc_md4", null ],
       [ "CMake dependency", "index.html#autotoc_md5", null ]
     ] ],
-    [ "Contributing", "index.html#autotoc_md6", [
-      [ "Building", "index.html#autotoc_md7", null ],
-      [ "Amalgamation", "index.html#autotoc_md8", null ],
-      [ "License", "index.html#autotoc_md9", null ]
+    [ "Installation", "index.html#autotoc_md6", [
+      [ "Homebrew", "index.html#autotoc_md7", null ]
+    ] ],
+    [ "Contributing", "index.html#autotoc_md8", [
+      [ "Building", "index.html#autotoc_md9", null ],
+      [ "Amalgamation", "index.html#autotoc_md10", null ],
+      [ "License", "index.html#autotoc_md11", null ]
     ] ],
     [ "Namespaces", "namespaces.html", [
       [ "Namespace List", "namespaces.html", "namespaces_dup" ],
