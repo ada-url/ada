379bf43cc3b3ac2dc0be91bdd6678de7ce9cc66f
Yagiz Nizipli <yagiz@nizipli.com>
2023-05-02 10:17:30 -0400

    feat: add setters to fuzzer

--- fuzz/parse.cc
+++ fuzz/parse.cc
@@ -7,18 +7,42 @@
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   FuzzedDataProvider fdp(data, size);
-  std::string source = fdp.ConsumeRandomLengthString(1024);
-  std::string base_source = fdp.ConsumeRandomLengthString(1024);
+  std::string source = fdp.ConsumeRandomLengthString();
+  std::string base_source = fdp.ConsumeRandomLengthString();
 
   /**
    * ada::parse<ada::url>
    */
-  ada::parse<ada::url>(source);
+  auto out_url = ada::parse<ada::url>(source);
+
+  if (out_url) {
+    std::string input = fdp.ConsumeRandomLengthString();
+    out_url->set_protocol(input);
+    out_url->set_username(input);
+    out_url->set_password(input);
+    out_url->set_hostname(input);
+    out_url->set_host(input);
+    out_url->set_pathname(input);
+    out_url->set_search(input);
+    out_url->set_hash(input);
+  }
 
   /**
    * ada::parse<ada::url_aggregator>
    */
-  ada::parse<ada::url_aggregator>(source);
+  auto out_aggregator = ada::parse<ada::url_aggregator>(source);
+
+  if (out_aggregator) {
+    std::string input = fdp.ConsumeRandomLengthString();
+    out_aggregator->set_protocol(input);
+    out_aggregator->set_username(input);
+    out_aggregator->set_password(input);
+    out_aggregator->set_hostname(input);
+    out_aggregator->set_host(input);
+    out_aggregator->set_pathname(input);
+    out_aggregator->set_search(input);
+    out_aggregator->set_hash(input);
+  }
 
   /**
    * ada::can_parse
