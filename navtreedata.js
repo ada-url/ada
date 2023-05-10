d7a7da70353ee2ad6d246e5735aee842e75e457d
anonrig <anonrig@users.noreply.github.com>
2023-05-10 23:32:37 +0000

    deploy: 616769dac3425d9a2b536ec26782b2a7c38b8bf0

--- navtreedata.js
+++ navtreedata.js
@@ -25,20 +25,21 @@
 var NAVTREE =
 [
   [ "Ada", "index.html", [
-    [ "Requirements", "index.html#autotoc_md5", null ],
-    [ "Usage", "index.html#autotoc_md6", [
-      [ "Parsing & Validation", "index.html#autotoc_md7", null ],
-      [ "Examples", "index.html#autotoc_md8", null ],
-      [ "C wrapper", "index.html#autotoc_md9", null ],
-      [ "CMake dependency", "index.html#autotoc_md10", null ]
+    [ "Ada is fast.", "index.html#autotoc_md5", null ],
+    [ "Requirements", "index.html#autotoc_md6", null ],
+    [ "Usage", "index.html#autotoc_md7", [
+      [ "Parsing & Validation", "index.html#autotoc_md8", null ],
+      [ "Examples", "index.html#autotoc_md9", null ],
+      [ "C wrapper", "index.html#autotoc_md10", null ],
+      [ "CMake dependency", "index.html#autotoc_md11", null ]
     ] ],
-    [ "Installation", "index.html#autotoc_md11", [
-      [ "Homebrew", "index.html#autotoc_md12", null ]
+    [ "Installation", "index.html#autotoc_md12", [
+      [ "Homebrew", "index.html#autotoc_md13", null ]
     ] ],
-    [ "Contributing", "index.html#autotoc_md13", [
-      [ "Building", "index.html#autotoc_md14", null ],
-      [ "Amalgamation", "index.html#autotoc_md15", null ],
-      [ "License", "index.html#autotoc_md16", null ]
+    [ "Contributing", "index.html#autotoc_md14", [
+      [ "Building", "index.html#autotoc_md15", null ],
+      [ "Amalgamation", "index.html#autotoc_md16", null ],
+      [ "License", "index.html#autotoc_md17", null ]
     ] ],
     [ "Command line options", "db/d8a/md_docs_cli.html", null ],
     [ "Namespaces", "namespaces.html", [
