9d39cb6861a8d91b04452df08e60b2dbdb459fe3
Daniel Lemire <daniel@lemire.me>
2023-05-11 08:41:17 -0400

    Update implementation.h

--- include/ada/implementation.h
+++ include/ada/implementation.h
@@ -24,11 +24,11 @@ using result = tl::expected<result_type, ada::errors>;
 
 /**
  * The URL parser takes a scalar value string input, with an optional null or
- * base URL base (default null). The parser assumes the input has an UTF-8
- * encoding.
+ * base URL base (default null). The parser assumes the input is a valid ASCII or UTF-8
+ * string.
  *
- * @param input the string input to analyze.
- * @param base_url the optional string input to use as a base url.
+ * @param input the string input to analyze (must be valid ASCII or UTF-8)
+ * @param base_url the optional URL input to use as a base url.
  * @return a parsed URL.
  */
 template <class result_type = ada::url_aggregator>
@@ -41,6 +41,8 @@ extern template ada::result<url_aggregator> parse<url_aggregator>(
     std::string_view input, const url_aggregator* base_url);
 
 /**
+ * Verifies whether the URL strings can be parsed. The function assumes 
+ * that the inputs are valid ASCII or UTF-8 strings.
  * @see https://url.spec.whatwg.org/#dom-url-canparse
  * @return If URL can be parsed or not.
  */
@@ -48,7 +50,8 @@ bool can_parse(std::string_view input,
                const std::string_view* base_input = nullptr);
 
 /**
- * Computes a href string from a file path.
+ * Computes a href string from a file path. The function assumes 
+ * that the input is a valid ASCII or UTF-8 string.
  * @return a href string (starts with file:://)
  */
 std::string href_from_file(std::string_view path);
