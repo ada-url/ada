8eb2aecc1d077cf4cb510eccf4a449e7dd65296e
Daniel Lemire <daniel@lemire.me>
2024-02-02 14:18:40 -0500

    setting version to 15

--- tools/run-clangcldocker.sh
+++ tools/run-clangcldocker.sh
@@ -5,10 +5,10 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 MAINSOURCE=$SCRIPTPATH/..
 ALL_ADA_FILES=$(cd $MAINSOURCE && git ls-tree --full-tree --name-only -r HEAD | grep -e ".*\.\(c\|h\|cc\|cpp\|hh\)\$" | grep -vFf clang-format-ignore.txt)
 
-if clang-format-17 --version | grep -qF 'version 17.'; then
-  clang-format-17 --style=file --verbose -i "$@" $ALL_ADA_FILES
+if clang-format-15 --version | grep -qF 'version 15.'; then
+  clang-format-15 --style=file --verbose -i "$@" $ALL_ADA_FILES
   exit 0
-elif clang-format --version | grep -qF 'version 17.'; then
+elif clang-format --version | grep -qF 'version 15.'; then
   clang-format --style=file --verbose -i "$@" $ALL_ADA_FILES
   exit 0
 fi
@@ -17,8 +17,8 @@ command -v docker >/dev/null 2>&1 || { echo >&2 "Please install docker. E.g., go
 docker info >/dev/null 2>&1 || { echo >&2 "Docker server is not running? type 'docker info'."; exit 1; }
 
 if [ -t 0 ]; then DOCKER_ARGS=-it; fi
-docker pull kszonek/clang-format-17
+docker pull kszonek/clang-format-15
 
-docker run --rm $DOCKER_ARGS -v "$MAINSOURCE":"$MAINSOURCE":Z  -w "$MAINSOURCE" -u "$(id -u $USER):$(id -g $USER)" kszonek/clang-format-17 --style=file --verbose -i "$@" $ALL_ADA_FILES
+docker run --rm $DOCKER_ARGS -v "$MAINSOURCE":"$MAINSOURCE":Z  -w "$MAINSOURCE" -u "$(id -u $USER):$(id -g $USER)" kszonek/clang-format-15 --style=file --verbose -i "$@" $ALL_ADA_FILES
 
 
