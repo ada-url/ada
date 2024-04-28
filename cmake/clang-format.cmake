1a80915aee457d90d465f240a785f2187d3ea406
Timmy <58427647+iamtimmy@users.noreply.github.com>
2024-04-28 16:47:22 +0200

    update clang-format.cmake to stop cmake configuration from emitting a warning when python is not found, even when the linting and format checks should not run. (#636)

--- cmake/clang-format.cmake
+++ cmake/clang-format.cmake
@@ -1,37 +1,41 @@
-find_program(PYTHON_EXECUTABLE python3 python)
-
-if(NOT PYTHON_EXECUTABLE)
-  message(WARNING "Python not found. Skipping lint and format checks.")
+if(NOT DEFINED ENV{LINT_AND_FORMAT_CHECK} AND NOT DEFINED ENV{FORMAT_ENABLED} )
+  # empty early exit, do not look for python
 else()
-  set(LINT_AND_FORMAT_SCRIPT_PATH ${CMAKE_SOURCE_DIR}/tools/lint_and_format.py)
+  find_program(PYTHON_EXECUTABLE python3 python)
 
-  # Should run on CI
-  if(DEFINED ENV{LINT_AND_FORMAT_CHECK})
-    message(STATUS "Checking code with clang-format...")
-    execute_process(
-      COMMAND ${PYTHON_EXECUTABLE} ${LINT_AND_FORMAT_SCRIPT_PATH} check
-      WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-      RESULT_VARIABLE clang_check_result
-    )
-
-    if(clang_check_result)
-      message(FATAL_ERROR "Clang-format check failed with error code ${clang_check_result}")
-    endif()
-
+  if(NOT PYTHON_EXECUTABLE)
+    message(WARNING "Python not found. Skipping lint and format checks.")
   else()
-    if(DEFINED ENV{FORMAT_ENABLED})
-      message(STATUS "Formatting code with clang-format...")
+    set(LINT_AND_FORMAT_SCRIPT_PATH ${CMAKE_SOURCE_DIR}/tools/lint_and_format.py)
+
+    # Should run on CI
+    if(DEFINED ENV{LINT_AND_FORMAT_CHECK})
+      message(STATUS "Checking code with clang-format...")
       execute_process(
-        COMMAND ${PYTHON_EXECUTABLE} ${LINT_AND_FORMAT_SCRIPT_PATH} format
-        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-        RESULT_VARIABLE clang_format_result
+              COMMAND ${PYTHON_EXECUTABLE} ${LINT_AND_FORMAT_SCRIPT_PATH} check
+              WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+              RESULT_VARIABLE clang_check_result
       )
 
-      if(clang_format_result)
-        message(FATAL_ERROR "Clang-format format failed with error code ${clang_check_result}")
+      if(clang_check_result)
+        message(FATAL_ERROR "Clang-format check failed with error code ${clang_check_result}")
       endif()
+
     else()
-      message(STATUS "Code formatting is not enabled.")
+      if(DEFINED ENV{FORMAT_ENABLED})
+        message(STATUS "Formatting code with clang-format...")
+        execute_process(
+                COMMAND ${PYTHON_EXECUTABLE} ${LINT_AND_FORMAT_SCRIPT_PATH} format
+                WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+                RESULT_VARIABLE clang_format_result
+        )
+
+        if(clang_format_result)
+          message(FATAL_ERROR "Clang-format format failed with error code ${clang_check_result}")
+        endif()
+      else()
+        message(STATUS "Code formatting is not enabled.")
+      endif()
     endif()
   endif()
 endif()
