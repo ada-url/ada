8b0dd6b3370225edde7f12619ce6462dbd9c22bc
Daniel Lemire <daniel@lemire.me>
2024-05-01 14:35:51 -0400

    fix: linting

--- src/unicode.cpp
+++ src/unicode.cpp
@@ -72,7 +72,7 @@ ada_really_inline bool has_tabs_or_newline(
    * credit for algorithmic idea: @aqrit, credit for description:
    * @DenisYaroshevskiy
    */
-  static uint8_t rnt_array[16] = {1, 0,  0, 0, 0,  0, 0, 0,
+  static uint8_t rnt_array[16] = {1, 0, 0,  0, 0, 0,  0, 0,
                                   0, 9, 10, 0, 0, 13, 0, 0};
   const uint8x16_t rnt = vld1q_u8(rnt_array);
   // m['0xd', '0xa', '0x9']
