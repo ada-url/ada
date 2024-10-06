a65c34608601f034295922748f31d840d5d0f804
Yagiz Nizipli <yagiz@nizipli.com>
2024-10-05 20:04:49 -0400

    fix aarch64 build errors

--- include/ada/helpers.h
+++ include/ada/helpers.h
@@ -102,7 +102,7 @@ bool overlaps(std::string_view input1, const std::string& input2) noexcept;
  * Return the substring from input going from index pos1 to the pos2 (non
  * included). The length of the substring is pos2 - pos1.
  */
-ada_really_inline constexpr std::string_view substring(const std::string& input,
+ada_really_inline constexpr std::string_view substring(std::string_view input,
                                                        size_t pos1,
                                                        size_t pos2) noexcept {
 #if ADA_DEVELOPMENT_CHECKS
@@ -112,7 +112,7 @@ ada_really_inline constexpr std::string_view substring(const std::string& input,
     abort();
   }
 #endif
-  return std::string_view(input.data() + pos1, pos2 - pos1);
+  return input.substr(pos1, pos2 - pos1);
 }
 
 /**
