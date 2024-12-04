acef83eca2af1d8e53acb2fe0b4777d77d23d15b
Yagiz Nizipli <yagiz@nizipli.com>
2024-12-03 20:41:33 -0500

    add ada::parse_url_pattern function

--- include/ada/parser.h
+++ include/ada/parser.h
@@ -7,10 +7,12 @@
 
 #include <optional>
 #include <string_view>
+#include <variant>
 
 #include "ada/encoding_type.h"
 #include "ada/expected.h"
 #include "ada/state.h"
+#include "ada/url_pattern.h"
 
 /**
  * @private
@@ -44,10 +46,16 @@ template <typename result_type = ada::url_aggregator, bool store_values = true>
 result_type parse_url_impl(std::string_view user_input,
                            const result_type* base_url = nullptr);
 
+tl::expected<ada::URLPattern, ada::url_pattern::errors> parse_url_pattern(
+    std::variant<std::string_view, URLPattern::Init> input,
+    const std::string_view* base_url = nullptr,
+    const ada::URLPattern::Options* options = nullptr);
+
 extern template url_aggregator parse_url_impl<url_aggregator>(
     std::string_view user_input, const url_aggregator* base_url);
 extern template url parse_url_impl<url>(std::string_view user_input,
                                         const url* base_url);
+
 }  // namespace ada::parser
 
 #endif  // ADA_PARSER_H
