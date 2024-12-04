fe3af1341e129718ea969ffcd908d81469f43b65
Yagiz Nizipli <yagiz@nizipli.com>
2024-12-04 11:40:32 -0500

    add initial parser_url_pattern method

--- include/ada/parser.h
+++ include/ada/parser.h
@@ -5,13 +5,10 @@
 #ifndef ADA_PARSER_H
 #define ADA_PARSER_H
 
-#include <optional>
 #include <string_view>
 #include <variant>
 
-#include "ada/encoding_type.h"
 #include "ada/expected.h"
-#include "ada/state.h"
 #include "ada/url_pattern.h"
 
 /**
@@ -33,7 +30,7 @@ namespace ada::parser {
  * parameter that can be used to resolve relative URLs. If the base_url is
  * provided, the user_input is resolved against the base_url.
  */
-template <typename result_type = ada::url_aggregator>
+template <typename result_type = url_aggregator>
 result_type parse_url(std::string_view user_input,
                       const result_type* base_url = nullptr);
 
@@ -42,14 +39,14 @@ extern template url_aggregator parse_url<url_aggregator>(
 extern template url parse_url<url>(std::string_view user_input,
                                    const url* base_url);
 
-template <typename result_type = ada::url_aggregator, bool store_values = true>
+template <typename result_type = url_aggregator, bool store_values = true>
 result_type parse_url_impl(std::string_view user_input,
                            const result_type* base_url = nullptr);
 
-tl::expected<ada::URLPattern, ada::url_pattern::errors> parse_url_pattern(
+tl::expected<URLPattern, url_pattern::errors> parse_url_pattern(
     std::variant<std::string_view, URLPattern::Init> input,
     const std::string_view* base_url = nullptr,
-    const ada::URLPattern::Options* options = nullptr);
+    const URLPattern::Options* options = nullptr);
 
 extern template url_aggregator parse_url_impl<url_aggregator>(
     std::string_view user_input, const url_aggregator* base_url);
