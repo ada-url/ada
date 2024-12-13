3c913be316be369fd34b4e7fb1d6acafcb1d03ab
Yagiz Nizipli <yagiz@nizipli.com>
2024-12-13 15:46:55 -0500

    add wpt_urlpattern_tests skeleton

---
+++ tests/wpt_urlpattern_tests.cpp
@@ -0,0 +1,8 @@
+#include "gtest/gtest.h"
+
+// Tests are taken from WPT
+// https://github.com/web-platform-tests/wpt/blob/master/urlpattern/resources/urlpattern-hasregexpgroups-tests.js
+TEST(wpt_urlpattern_tests, has_regexp_groups) {
+  // TODO: Implement this.
+  SUCCEED();
+}
