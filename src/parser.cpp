671898bdf263bbb1e6698bb8b10f8f3a96da97f9
Yagiz Nizipli <yagiz@nizipli.com>
2024-12-16 13:16:25 -0500

    more progress on missing features

--- src/parser.cpp
+++ src/parser.cpp
@@ -950,15 +950,15 @@ tl::expected<url_pattern, url_pattern_errors> parse_url_pattern_impl(
   // For each componentName of « "protocol", "username", "password", "hostname",
   // "port", "pathname", "search", "hash" If processedInit[componentName] does
   // not exist, then set processedInit[componentName] to "*".
-  if (!processed_init->protocol.has_value()) processed_init->protocol = "*";
-  if (!processed_init->username.has_value()) processed_init->username = "*";
-  if (!processed_init->username.has_value()) processed_init->username = "*";
-  if (!processed_init->password.has_value()) processed_init->password = "*";
-  if (!processed_init->hostname.has_value()) processed_init->hostname = "*";
-  if (!processed_init->port.has_value()) processed_init->port = "*";
-  if (!processed_init->pathname.has_value()) processed_init->pathname = "*";
-  if (!processed_init->search.has_value()) processed_init->search = "*";
-  if (!processed_init->hash.has_value()) processed_init->hash = "*";
+  if (!processed_init->protocol) processed_init->protocol = "*";
+  if (!processed_init->username) processed_init->username = "*";
+  if (!processed_init->username) processed_init->username = "*";
+  if (!processed_init->password) processed_init->password = "*";
+  if (!processed_init->hostname) processed_init->hostname = "*";
+  if (!processed_init->port) processed_init->port = "*";
+  if (!processed_init->pathname) processed_init->pathname = "*";
+  if (!processed_init->search) processed_init->search = "*";
+  if (!processed_init->hash) processed_init->hash = "*";
 
   // If processedInit["protocol"] is a special scheme and processedInit["port"]
   // is a string which represents its corresponding default port in radix-10
@@ -974,26 +974,38 @@ tl::expected<url_pattern, url_pattern_errors> parse_url_pattern_impl(
   // Set urlPattern’s protocol component to the result of compiling a component
   // given processedInit["protocol"], canonicalize a protocol, and default
   // options.
-  url_pattern_.protocol_component = url_pattern_component::compile(
+  auto protocol_component = url_pattern_component::compile(
       processed_init->protocol.value(),
       url_pattern_helpers::canonicalize_protocol,
       url_pattern_compile_component_options::DEFAULT);
+  if (!protocol_component) {
+    return tl::unexpected(protocol_component.error());
+  }
+  url_pattern_.protocol_component = std::move(*protocol_component);
 
   // Set urlPattern’s username component to the result of compiling a component
   // given processedInit["username"], canonicalize a username, and default
   // options.
-  url_pattern_.username_component = url_pattern_component::compile(
+  auto username_component = url_pattern_component::compile(
       processed_init->username.value(),
       url_pattern_helpers::canonicalize_username,
       url_pattern_compile_component_options::DEFAULT);
+  if (!username_component) {
+    return tl::unexpected(username_component.error());
+  }
+  url_pattern_.username_component = std::move(*username_component);
 
   // Set urlPattern’s password component to the result of compiling a component
   // given processedInit["password"], canonicalize a password, and default
   // options.
-  url_pattern_.password_component = url_pattern_component::compile(
+  auto password_component = url_pattern_component::compile(
       processed_init->password.value(),
       url_pattern_helpers::canonicalize_password,
       url_pattern_compile_component_options::DEFAULT);
+  if (!password_component) {
+    return tl::unexpected(password_component.error());
+  }
+  url_pattern_.password_component = std::move(*password_component);
 
   // TODO: Optimization opportunity. The following if statement can be
   // simplified.
@@ -1002,25 +1014,37 @@ tl::expected<url_pattern, url_pattern_errors> parse_url_pattern_impl(
   // to the result of compiling a component given processedInit["hostname"],
   // canonicalize an IPv6 hostname, and hostname options.
   if (url_pattern_helpers::is_ipv6_address(processed_init->hostname.value())) {
-    url_pattern_.hostname_component = url_pattern_component::compile(
+    auto hostname_component = url_pattern_component::compile(
         processed_init->hostname.value(),
         url_pattern_helpers::canonicalize_hostname,
         url_pattern_compile_component_options::DEFAULT);
+    if (!hostname_component) {
+      return tl::unexpected(hostname_component.error());
+    }
+    url_pattern_.hostname_component = std::move(*hostname_component);
   } else {
     // Otherwise, set urlPattern’s hostname component to the result of compiling
     // a component given processedInit["hostname"], canonicalize a hostname, and
     // hostname options.
-    url_pattern_.hostname_component = url_pattern_component::compile(
+    auto hostname_component = url_pattern_component::compile(
         processed_init->hostname.value(),
         url_pattern_helpers::canonicalize_hostname,
         url_pattern_compile_component_options::HOSTNAME);
+    if (!hostname_component) {
+      return tl::unexpected(hostname_component.error());
+    }
+    url_pattern_.hostname_component = std::move(*hostname_component);
   }
 
   // Set urlPattern’s port component to the result of compiling a component
   // given processedInit["port"], canonicalize a port, and default options.
-  url_pattern_.port_component = url_pattern_component::compile(
+  auto port_component = url_pattern_component::compile(
       processed_init->port.value(), url_pattern_helpers::canonicalize_port,
       url_pattern_compile_component_options::DEFAULT);
+  if (!port_component) {
+    return tl::unexpected(port_component.error());
+  }
+  url_pattern_.port_component = std::move(*port_component);
 
   // Let compileOptions be a copy of the default options with the ignore case
   // property set to options["ignoreCase"].
@@ -1044,29 +1068,45 @@ tl::expected<url_pattern, url_pattern_errors> parse_url_pattern_impl(
     // Set urlPattern’s pathname component to the result of compiling a
     // component given processedInit["pathname"], canonicalize a pathname, and
     // pathCompileOptions.
-    url_pattern_.pathname_component = url_pattern_component::compile(
+    auto pathname_component = url_pattern_component::compile(
         processed_init->pathname.value(),
         url_pattern_helpers::canonicalize_pathname, path_compile_options);
+    if (!pathname_component) {
+      return tl::unexpected(pathname_component.error());
+    }
+    url_pattern_.pathname_component = std::move(*pathname_component);
   } else {
     // Otherwise set urlPattern’s pathname component to the result of compiling
     // a component given processedInit["pathname"], canonicalize an opaque
     // pathname, and compileOptions.
-    url_pattern_.pathname_component = url_pattern_component::compile(
+    auto pathname_component = url_pattern_component::compile(
         processed_init->pathname.value(),
         url_pattern_helpers::canonicalize_opaque_pathname, compile_options);
+    if (!pathname_component) {
+      return tl::unexpected(pathname_component.error());
+    }
+    url_pattern_.pathname_component = std::move(*pathname_component);
   }
 
   // Set urlPattern’s search component to the result of compiling a component
   // given processedInit["search"], canonicalize a search, and compileOptions.
-  url_pattern_.search_component = url_pattern_component::compile(
+  auto search_component = url_pattern_component::compile(
       processed_init->search.value(), url_pattern_helpers::canonicalize_search,
       compile_options);
+  if (!search_component) {
+    return tl::unexpected(search_component.error());
+  }
+  url_pattern_.search_component = std::move(*search_component);
 
   // Set urlPattern’s hash component to the result of compiling a component
   // given processedInit["hash"], canonicalize a hash, and compileOptions.
-  url_pattern_.hash_component = url_pattern_component::compile(
+  auto hash_component = url_pattern_component::compile(
       processed_init->hash.value(), url_pattern_helpers::canonicalize_hash,
       compile_options);
+  if (!hash_component) {
+    return tl::unexpected(hash_component.error());
+  }
+  url_pattern_.hash_component = std::move(*hash_component);
 
   // Return urlPattern.
   return url_pattern_;
