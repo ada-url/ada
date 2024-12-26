0f4ea6682e91db5002e8e9c5175a23302c31da73
Daniel Lemire <daniel@lemire.me>
2024-12-26 15:17:02 -0500

    fixing logging

--- tests/wpt_urlpattern_tests.cpp
+++ tests/wpt_urlpattern_tests.cpp
@@ -252,9 +252,7 @@ TEST(wpt_urlpattern_tests, urlpattern_test_data) {
         FAIL() << "Test should have succeeded but failed" << std::endl
                << main_object.raw_json().value() << std::endl;
       }
-#ifdef ADA_HAS_FORMAT
       ada_log("parse_result: ", parse_result->to_string());
-#endif
       ondemand::array exactly_empty_components;
       if (!main_object["exactly_empty_components"].get_array().get(
               exactly_empty_components)) {
