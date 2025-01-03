b31c2500b8ef90f82e1615e53f25ffc6c192569f
Daniel Lemire <dlemire@lemire.me>
2025-01-03 13:02:47 -0500

    reduction

--- tests/wpt_urlpattern_tests.cpp
+++ tests/wpt_urlpattern_tests.cpp
@@ -1,12 +1,7 @@
-#include <filesystem>
-#include <iostream>
+#include "gtest/gtest.h"
 
- #include "gtest/gtest.h"
-
- #include "ada.h"
+#include "ada.h"
 
 TEST(wpt_urlpattern_tests, parser_tokenize_basic_tests) {
-    auto tokenize_result =
-       tokenize("*", ada::url_pattern_helpers::token_policy::STRICT);
-    ASSERT_TRUE(tokenize_result);
+    auto tokenize_result = ada::url_pattern_helpers::tokenize();
 }
