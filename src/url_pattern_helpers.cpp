d073a48caf32172aa1497fdcfb64c42984293723
Daniel Lemire <dlemire@lemire.me>
2025-01-03 13:10:24 -0500

    simplify

--- src/url_pattern_helpers.cpp
+++ src/url_pattern_helpers.cpp
@@ -1,12 +1,8 @@
 #include "ada/url_pattern_helpers.h"
 
-#include <algorithm>
-#include <optional>
-#include <string>
-
 namespace ada::url_pattern_helpers {
 
-std::vector<int> tokenize(){
+std::vector<int> tokenize() {
   std::vector<int> token_list;
   return token_list;
 }
