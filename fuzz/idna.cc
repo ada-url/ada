d62f7c097c263ac2df254fdd190664f8944dee5c
Yagiz Nizipli <yagiz@nizipli.com>
2025-01-20 14:06:44 -0500

    try to fix fuzzer

--- fuzz/idna.cc
+++ fuzz/idna.cc
@@ -9,7 +9,6 @@
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   FuzzedDataProvider fdp(data, size);
   std::string source = fdp.ConsumeRandomLengthString(256);
-  std::string base_source = fdp.ConsumeRandomLengthString(256);
 
   /**
    * ada::idna
