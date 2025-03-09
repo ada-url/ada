fde4121f0738344087e133a3d38ab62512e46caa
Daniel Lemire <daniel@lemire.me>
2025-03-09 14:18:14 -0400

    removing the declaration of ascii_has_upper_case

--- include/ada/ada_idna.h
+++ include/ada/ada_idna.h
@@ -35,8 +35,6 @@ namespace ada::idna {
 
 // If the input is ascii, then the mapping is just -> lower case.
 void ascii_map(char* input, size_t length);
-// check whether an ascii string needs mapping
-bool ascii_has_upper_case(char* input, size_t length);
 // Map the characters according to IDNA, returning the empty string on error.
 std::u32string map(std::u32string_view input);
 
