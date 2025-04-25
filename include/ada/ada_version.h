2571b46eb0e3f5bc8d16d33fa491c38628d0ba4e
anonrig <anonrig@users.noreply.github.com>
2025-04-25 00:02:47 +0000

    chore: release v3.2.3

--- include/ada/ada_version.h
+++ include/ada/ada_version.h
@@ -5,14 +5,14 @@
 #ifndef ADA_ADA_VERSION_H
 #define ADA_ADA_VERSION_H
 
-#define ADA_VERSION "3.2.2"
+#define ADA_VERSION "3.2.3"
 
 namespace ada {
 
 enum {
   ADA_VERSION_MAJOR = 3,
   ADA_VERSION_MINOR = 2,
-  ADA_VERSION_REVISION = 2,
+  ADA_VERSION_REVISION = 3,
 };
 
 }  // namespace ada
